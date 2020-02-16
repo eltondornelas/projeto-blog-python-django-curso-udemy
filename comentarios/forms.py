7665135a53ec0f02648c332e3d3b47bdcef1680b
Elton Dornelas <eltondornelas@gmail.com>
2020-02-16 13:52:36 -0300

    Adicionando reCAPTCHA

--- comentarios/forms.py
+++ comentarios/forms.py
@@ -1,16 +1,49 @@
 from django.forms import ModelForm
 from .models import Comentario
+import requests
 
 
 class FormComentario(ModelForm):
 
     def clean(self):
+        # validando o captcha primeiro
+        raw_data = self.data
+        recaptcha_response = raw_data.get('g-recaptcha-response')
+
+        ''' essa validação não tem mais sentido, pois a debaixo faz essa validação com a chave também
+        if not recaptcha_response:
+            self.add_error(
+                'comentario', 'Por favor, marque a caixa "Não sou um robô".'
+            )
+        '''
+        
+        # https://google.com/recaptcha/api/siteverify
+        # secret_key
+        # resposta_captcha
+
+        recaptcha_request = requests.post(
+            'https://google.com/recaptcha/api/siteverify',
+            data={
+                'secret': '6LfhYtkUAAAAAGvSkHCzmYaVHcK357qLhTgbR-eW',  # secret_key do backend
+                'response': recaptcha_response
+            }
+        )
+        
+        recaptcha_result = recaptcha_request.json()
+
+        if not recaptcha_result.get('success'):
+            self.add_error(
+                'comentario', 'Ocorreu um erro.'
+            )
+        
+        # print(recaptcha_result)
+
         # o clean coloca as validações para aparecer as msgs de erro no seu devido campo, precisa pegar os dados primeiro
-        data = self.cleaned_data
+        cleaned_data = self.cleaned_data
         # print(data)
-        nome = data.get('nome_comentario')
-        email = data.get('email_comentario')
-        comentario = data.get('comentario')
+        nome = cleaned_data.get('nome_comentario')
+        email = cleaned_data.get('email_comentario')
+        comentario = cleaned_data.get('comentario')
 
         if len(nome) < 5:
             self.add_error(
